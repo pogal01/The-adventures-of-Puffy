3fe5d5b923503e366b00b6f4e154925bfdf95f7e
pogal01 <pogal2000@gmail.com>
2022-03-07 15:55:35 +0000

    Eye follows and other stuff

--- Assets/Expressions.cs
+++ Assets/Expressions.cs
@@ -6,7 +6,8 @@ public class Expressions : MonoBehaviour
 {
    
     [HideInInspector]  public GameObject [] TheExpressions;
-
+    [HideInInspector]  public GameObject[] TheEyes;
+   
 
     public enum Faces
     {
@@ -16,13 +17,26 @@ public class Expressions : MonoBehaviour
         Normal,
         Closed
     }
-    public Faces Expression;
+    public enum Eyes
+    {
+        Normal,
+        Happy
+    }
 
+    public Faces Expression;
+    public Eyes eyes;
+
+    private Faces CurrentFace;
+    private Eyes CurrentEyes;
 
     // Start is called before the first frame update
     void Start()
     {
+        eyes = Eyes.Normal;
         Expression = Faces.Normal;
+        CurrentFace = Expression;
+        CurrentEyes = eyes;
+        ChangeExpression();
     }
 
     // Update is called once per frame
@@ -33,6 +47,24 @@ public class Expressions : MonoBehaviour
         Face.SetActive(true);
 
 
+    }
+
+    public void ActivateEyes(GameObject eyes)
+    {
+        
+        eyes.SetActive(true);
+
+
+    }
+
+    public void DisableEyes()
+    {
+        foreach (var Eyes in TheEyes)
+        {
+            Eyes.SetActive(false);
+
+
+        }
     }
 
     public void DisableFace()
@@ -46,33 +78,76 @@ public class Expressions : MonoBehaviour
     }
 
 
-
-    void Update()
+    private void ChangeExpression()
     {
         switch (Expression)
         {
             case Faces.Happy:
                 ActivateFace(TheExpressions[1]);
-             
+                CurrentFace = Expression;
+
                 break;
             case Faces.Sad:
-               ActivateFace(TheExpressions[3]);
-               
+                ActivateFace(TheExpressions[3]);
+                CurrentFace = Expression;
                 break;
             case Faces.Open:
                 ActivateFace(TheExpressions[4]);
-              
+                CurrentFace = Expression;
                 break;
             case Faces.Normal:
                 ActivateFace(TheExpressions[2]);
-               
+                CurrentFace = Expression;
                 break;
             case Faces.Closed:
                 ActivateFace(TheExpressions[0]);
-              
+                CurrentFace = Expression;
                 break;
             default:
                 break;
         }
+
+        switch (eyes)
+        {
+            case Eyes.Normal:
+                DisableEyes();
+                ActivateEyes(TheEyes[0]);
+                ActivateEyes(TheEyes[1]);
+                CurrentEyes = eyes;
+                Debug.Log("Normal Eyes");
+                break;
+            case Eyes.Happy:
+                DisableEyes();
+                ActivateEyes(TheEyes[2]);
+                ActivateEyes(TheEyes[3]);
+                CurrentEyes = eyes;
+                Debug.Log("Happy Eyes");
+                break;
+            default:
+                break;
+
+        }
     }
+
+
+
+    void Update()
+    {
+        
+
+        if (CurrentFace != Expression || CurrentEyes != eyes)
+        {
+            ChangeExpression();
+
+
+        }
+
+
+
+    }
+
+         
+
+
 }
+
