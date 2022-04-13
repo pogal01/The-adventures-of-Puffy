2c78e9b33aa2ff996052218e9342bb400bfc864d
ClebDoesStuff <ClebGaming@gmail.com>
2022-04-13 17:59:56 +0100

    Menu_Progression-3.0.0
    Added:
    - Pause Menu
    - Started Creation of Final Level

--- Assets/Scripts/Calebs_Scripts/Menu_Controller.cs
+++ Assets/Scripts/Calebs_Scripts/Menu_Controller.cs
@@ -13,10 +13,30 @@ public class Menu_Controller : MonoBehaviour
 
     Resolution[] resolutions;
 
+    public bool game_Is_Paused = false;
+    public GameObject pause_Menu_UI;
+    public GameObject pause_Button_Objects;
+    public GameObject settings_Objects;
+
     public void Start()
     {
         Load_Values();
         Get_Resolutions();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (game_Is_Paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
     }
 
     //---------- VOLUME --------------
@@ -80,11 +100,46 @@ public class Menu_Controller : MonoBehaviour
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
+    //-------------- PAUSE --------------
+    public void Resume()
+    {
+        Debug.Log("Game Resumed");
+        pause_Menu_UI.SetActive(false);
+        Time.timeScale = 1f;
+        game_Is_Paused = false;
+    }
+
+    public void Pause()
+    {
+        Debug.Log("Game Paused");
+        pause_Menu_UI.SetActive(true);
+        pause_Button_Objects.SetActive(true);
+        settings_Objects.SetActive(false);
+        Time.timeScale = 0f;
+        game_Is_Paused = true;
+    }
+
+    public void Paused_Settings_Open()
+    {
+        Debug.Log("Paused Settings Opened");
+        pause_Button_Objects.SetActive(false);
+        settings_Objects.SetActive(true);
+    }
+    
+    public void Paused_Settings_Closed()
+    {
+        Debug.Log("Paused Settings Closed");
+        pause_Button_Objects.SetActive(true);
+        settings_Objects.SetActive(false);
+    }
+
     void Load_Values()
     {
         float volume_Value = PlayerPrefs.GetFloat("Game_Volume");
         volume_Slider.value = volume_Value;
         AudioListener.volume = volume_Value;
     }
+
+
 
 }
