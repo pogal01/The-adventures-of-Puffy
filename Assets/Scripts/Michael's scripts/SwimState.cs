ef5ee6c1ca47e939705d1638333f30ced75c8e1e
pogal01 <pogal2000@gmail.com>
2022-04-15 15:46:03 +0100

    Fixed swimming in the air... Again

--- Assets/Scripts/Michael's scripts/SwimState.cs
+++ Assets/Scripts/Michael's scripts/SwimState.cs
@@ -6,11 +6,13 @@ public class SwimState : StateMachineBehaviour
 {
     private Expressions ExpressionsScript;
 	private CharacterControler CharacterControlerScript;
-	
+	private PlayerAnimationControler Animationscript;
+
 	private void Awake()
     {
         ExpressionsScript = GameObject.Find("Puffy").GetComponent<Expressions>();
 		CharacterControlerScript = GameObject.Find("Puffy").GetComponent<CharacterControler>();
+		Animationscript = GameObject.Find("Puffy").GetComponent<PlayerAnimationControler>();
 	}
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -28,12 +30,12 @@ public class SwimState : StateMachineBehaviour
 
 	public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 	{
-		if (CharacterControlerScript.PuffyState != CharacterControler.state.Swim)
+		if (CharacterControlerScript.PuffyState == CharacterControler.state.Swim)
 		{
-			CharacterControlerScript.PuffyState = CharacterControler.state.Swim;
-
-
-
+			if (CharacterControlerScript.LastCollider == GameObject.Find("Water_Tilemap").GetComponent<CompositeCollider2D>())
+			{
+				Animationscript.FlyAnimStart();
+			}
 		}
 	}
 
