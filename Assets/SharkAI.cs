d5f9c2c123c868f0585d4397e496c268b67ed358
pogal01 <pogal2000@gmail.com>
2022-04-20 01:07:27 +0100

    Finally fixed the shark

--- Assets/SharkAI.cs
+++ Assets/SharkAI.cs
@@ -13,6 +13,16 @@ public class SharkAI : MonoBehaviour
     private float AmountOfWaypoints;
     private Rigidbody2D rb;
     private Vector3 Rotation;
+    private Transform ThisObject;
+    private Transform TheRotation;
+
+    private Vector3 RotationToChangeTo;
+
+    //Debug
+    private GameObject TargetObject;
+   
+
+
     void Start()
     {
         Waypoints = GameObject.FindGameObjectsWithTag("SharkWaypoint" + SharkID);
@@ -20,7 +30,11 @@ public class SharkAI : MonoBehaviour
        // Debug.Log("The amount of waypoints for " + SharkID + " Is " + AmountOfWaypoints);
         WhereToGo = Waypoints[WaypointOrder].transform.position;
         rb = GetComponent<Rigidbody2D>();
-       
+        ThisObject = gameObject.transform;
+        TargetObject = Waypoints[WaypointOrder];
+        transform.right = TargetObject.transform.position - transform.position;
+        RotationToChangeTo = transform.right = TargetObject.transform.position - transform.position;
+        Rotation = transform.rotation.eulerAngles;
     }
 
     private void OnTriggerEnter2D(Collider2D Collision)
@@ -31,17 +45,20 @@ public class SharkAI : MonoBehaviour
         if (Collision.tag == ("SharkWaypoint" + SharkID))
         {
             Debug.Log("Shark has collided with waypoint");
+            
+            //Rotation = transform.rotation.eulerAngles;
 
-
-           
-            if (WaypointOrder != AmountOfWaypoints)
+            if (WaypointOrder != AmountOfWaypoints-1)
             {
               WaypointOrder = ++WaypointOrder;
                 
                 WhereToGo = Waypoints[WaypointOrder].transform.position;
                 Debug.Log("Waypoint updated");
-
-
+                TargetObject = Waypoints[WaypointOrder];
+                transform.right = TargetObject.transform.position - transform.position;
+                RotationToChangeTo = transform.right = TargetObject.transform.position - transform.position;
+                Rotation = transform.rotation.eulerAngles;
+                CheckSharkIsNotUpsidedown();
             }
             else
             {
@@ -49,7 +66,8 @@ public class SharkAI : MonoBehaviour
                 WaypointOrder = 0;
                 Debug.Log("Waypoint defaulted back to zero");
                 WhereToGo = Waypoints[WaypointOrder].transform.position; // resets the loop
-
+                RotationToChangeTo = transform.right = TargetObject.transform.position - transform.position;
+                CheckSharkIsNotUpsidedown();
 
             }
 
@@ -75,7 +93,20 @@ public class SharkAI : MonoBehaviour
 
     
         
+    void CheckSharkIsNotUpsidedown()
+    {
+        if(Rotation.z > 90 && Rotation.z < 270)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, 180, transform.rotation.z);
 
+            Debug.Log("The shark is upsidedown");
+
+
+        }
+
+
+
+    }
         
 
 
@@ -85,28 +116,55 @@ public class SharkAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Rotation = transform.rotation.eulerAngles;
-
-        if(Rotation.z > 90 && Rotation.z < 180)
-        {
-            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 180, transform.rotation.eulerAngles.z);
+        Debug.Log("Sharks velocity is" + rb.velocity);
 
 
+        
+        float RotationZ = Mathf.Clamp(ThisObject.rotation.z, -270f, -90f);
+
+        
+
+            /*
+            if (rb.velocity == Vector2.right)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
 
 
 
-        }
+            }
+            else if (rb.velocity == Vector2.left)
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
 
-        /*
-        if (WaypointOrder == AmountOfWaypoints)
-        {
-           
 
-        }
-        */
 
-        transform.position = Vector2.MoveTowards(transform.position, WhereToGo, Speed * Time.deltaTime); //Moves
-        transform.right = WhereToGo - transform.position; //rotates
+            }
+            else if (rb.velocity == Vector2.up)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 90);
+
+
+
+            }
+            else if (rb.velocity == Vector2.down)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, -90);
+
+
+
+            }
+            */
+            /*
+            if (WaypointOrder == AmountOfWaypoints)
+            {
+
+
+            }
+            */
+
+            //Moves
+            transform.position = Vector2.MoveTowards(transform.position, WhereToGo, Speed * Time.deltaTime);
+      
 
         /*
         if(transform.rotation.eulerAngles.z > 90)
